ad3ebdb0864473c0c1bf3b3e7a7a78202d802748
rekhazim <rekhagopinath1992@gmail.com>
2022-09-26 16:18:34 +0530

    AAA

---
+++ src/org/sample/Student.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class Student {
+	public static void main(String[] args) {
+		
+		for(int i=0; i<10; i++) {
+			if(i==5);{
+				continue;
+			}
+		}
+		System.out.println("java selenium");
+	}
+	}
+
+
